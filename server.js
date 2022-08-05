3eb7302283c1044d530b4d31d1722950d4dbdf1c
Jerry Hsu <jerryhsu@hey.com>
2022-08-05 17:47:37 -0500

    check request body

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const koa = require('koa')
+const bodyParser = require('koa-bodyparser');
+
 const app = new koa()
+app.use(bodyParser());
 
 const body = {
     name: "hello from Jerry"
 }
 
 app.use(async (ctx) => {
+    console.log('0000', ctx.request.body)
     ctx.body = JSON.stringify(body)
 })
 
