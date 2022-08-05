53abc01b128835c0d366764f7c57c0e5ae5312d7
Jerry Hsu <jerryhsu@hey.com>
2022-08-05 17:28:14 -0500

    add scripts

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ app.use(async (ctx) => {
     ctx.body = 'Hello World'
 })
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Listening on port ${port}`))
